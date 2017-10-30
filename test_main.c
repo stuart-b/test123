8e95c147fff92146771ed2d4ec6dcb6256472488
Stuart <stuart.begg@gmail.com>
2017-10-30 16:22:34 +0000

    Update test_main.c

--- test_main.c
+++ test_main.c
@@ -5,26 +5,12 @@
 int main(void)
 {
   int i;
-  char* a = "1234567890\0";
+  //char* a = "1234567890\0";
+  char a[5];
   char b[5];
-  int *array = malloc(5 * sizeof(int));
-
-  if (array != NULL) 
-  {
-    array[0] = 13;
-    array[1] = 7;
-    array[2] = 42;
-    array[3] = 0;
-    array[4] = 1;
-
-    for (i = 0; i < 5; i++) 
-    {
-          printf("array[%d] = %d\n", i, array[i]);
-    }
-  }
-    
-  (void) strcpy(a, b);
-  printf ("--%s-- in and --%s-- out", a,b);
+  a[5] = 'a';
+  b[5] = 'b';
+  
 
   return 0;
 }
