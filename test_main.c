d5f22de0fe0fe1a925d86472ae4f8f41f6c48b9c
Stuart <stuart.begg@gmail.com>
2017-10-30 16:52:43 +0000

    Update test_main.c

--- test_main.c
+++ test_main.c
@@ -2,23 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
-extern int k(int *a);
-
-int func(int argc)
-{
-  unsigned int a = 512;
-  int b = 4;
-  int c;
-  short d = 0;
-  d = k(&c);
-  //d = c + 0;
-  return 4;
 }
 
 
 
 int main(int argc, char** argv)
 {
-  func(argc);
+
+  char* a = "abcdefg";
+  char b[2];
+  
+  sscanf(a, "%s", b);
+  
+  
   return 0;
 }
