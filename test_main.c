82db900d192b222098549b152e48d9c41efb503d
Stuart <stuart.begg@gmail.com>
2017-10-30 16:28:49 +0000

    Update test_main.c

--- test_main.c
+++ test_main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char** argv)
+int func(int argc)
 {
   char a[10];
   char b[10];
@@ -12,5 +12,13 @@ int main(int argc, char** argv)
     a[i] = b[i];
     i++;
   }
+  return 42;
+}
+
+
+
+int main(int argc, char** argv)
+{
+  func(100);
   return 0;
 }
