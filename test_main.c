b9379379d2c6e722312fb3a7f7d2d29a7f7a51e7
Stuart <stuart.begg@gmail.com>
2017-10-30 16:46:35 +0000

    Update test_main.c

--- test_main.c
+++ test_main.c
@@ -2,14 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
+int k(int *a)
+{
+  int b = a;
+  return b;
+}
+
 int func(int argc)
 {
   unsigned int a = 512;
   int b = 4;
   int c;
   short d = 0;
-  
-  d = c + 0;
+  d = k(&b);
+  //d = c + 0;
   return 4;
 }
 
