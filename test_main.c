9da47aef194d10899a9d4360eddfca02e49a5b93
Stuart <stuart.begg@gmail.com>
2017-10-31 09:12:05 +0000

    Update test_main.c

--- test_main.c
+++ test_main.c
@@ -2,14 +2,32 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char** argv)
+int error(int arg)
 {
-
-  char* a = "abcdefg";
+  char a[2];
   char b[2];
   
-  sscanf(a, "%s", b);
+  a[arg] = 'm';
   
+  return argc;
+}
+
+int error2(int arg)
+{
+  char a[2];
+  char b[2];
+  if (argc > 10)
+  {
+    a[arg] = 'm';
+  }
+  return argc;
+}
+
+
+int main(int argc, char** argv)
+{
   
+  error(argc);
+  error2(argc);
   return 0;
 }
